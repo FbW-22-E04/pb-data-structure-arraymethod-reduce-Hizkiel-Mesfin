df6babcc35aa55706020b5f1c81840cf4d96ccb2
Hizkiel-Mesfin <mesfinezekiel@gmail.com>
2022-08-17 15:50:01 +0200

    reduced hahah

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-
 //Create a function groupById(arr) that creates an object from it, with id as the key, and array items as values.
 //1
 
 let users = [
-  {id: 'john', name: "John Smith", age: 20},
-  {id: 'ann', name: "Ann Smith", age: 24},
-  {id: 'pete', name: "Pete Peterson", age: 31},
+  { id: "john", name: "John Smith", age: 20 },
+  { id: "ann", name: "Ann Smith", age: 24 },
+  { id: "pete", name: "Pete Peterson", age: 31 },
 ];
 
-function groupById(array) {
-  //write your code here
+function groupById(arr) {
+  const chris = arr.reduce((acc, next) => {
+    let firstName = next.id;
+    acc[firstName] = next;
+    return acc;
+  }, {});
+
+  return chris;
 }
 
-let usersById= groupById(users)
-console.log(usersById)
-
+console.log(groupById(users));
 /* expected output: 
 
 {
